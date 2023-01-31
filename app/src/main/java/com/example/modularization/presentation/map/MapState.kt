30ee8ad44ad871917dcf71c403f8f0e5c2ac5f4b
BENJAH <benjahjp@gmail.com>
2023-01-31 11:14:03 +0300

    feat: map state

---
+++ app/src/main/java/com/example/modularization/presentation/map/MapState.kt
@@ -0,0 +1,7 @@
+package com.example.modularization.presentation.map
+
+import com.google.maps.android.compose.MapProperties
+
+data class MapState(
+    val properties: MapProperties = MapProperties()
+)
