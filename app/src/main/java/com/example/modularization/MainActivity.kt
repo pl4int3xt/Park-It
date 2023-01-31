059e3573c4ca09a911943ba7b73c13fa1c27e367
BENJAH <benjahjp@gmail.com>
2023-01-31 14:35:12 +0300

    feat: permissions

--- app/src/main/java/com/example/modularization/MainActivity.kt
+++ app/src/main/java/com/example/modularization/MainActivity.kt
@@ -3,22 +3,19 @@ package com.example.modularization
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.view.WindowCompat
 import com.example.modularization.presentation.map.components.MapScreen
 import com.example.modularization.ui.theme.ModularizationTheme
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
         WindowCompat.setDecorFitsSystemWindows(window, false)
         setContent {
             ModularizationTheme {
--- app/src/main/java/com/example/modularization/presentation/map/components/MapScreen.kt
+++ app/src/main/java/com/example/modularization/presentation/map/components/MapScreen.kt
@@ -1,18 +1,25 @@
 package com.example.modularization.presentation.map.components
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.LocationOn
 import androidx.compose.material.icons.filled.MyLocation
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.modularization.presentation.map.MapEvent
 import com.example.modularization.presentation.map.MapScreenViewModel
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.isGranted
+import com.google.accompanist.permissions.rememberPermissionState
+import com.google.accompanist.permissions.shouldShowRationale
 import com.google.android.gms.maps.model.BitmapDescriptor
 import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
@@ -21,20 +28,48 @@ import com.google.maps.android.compose.MapUiSettings
 import com.google.maps.android.compose.Marker
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
-@OptIn(ExperimentalMaterial3Api::class)
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalPermissionsApi::class)
 @Composable
 fun MapScreen(
     viewModel: MapScreenViewModel = hiltViewModel()
 ) {
+    val locationPermissionState = rememberPermissionState(
+        permission = android.Manifest.permission.ACCESS_FINE_LOCATION
+    )
+
     val mapUiSettings = remember{ MapUiSettings(zoomControlsEnabled = false) }
     Scaffold(
         floatingActionButton = {
-            FloatingActionButton(onClick = { /*TODO*/ }) {
+            FloatingActionButton(onClick = {
+            }) {
                 Icon(imageVector = Icons.Default.MyLocation,
                     contentDescription = "my location")
             }
         }
     ) {
+
+        if (locationPermissionState.status.isGranted) {
+
+        } else {
+            Column {
+                val textToShow = if (locationPermissionState.status.shouldShowRationale) {
+                    // If the user has denied the permission but the rationale can be shown,
+                    // then gently explain why the app requires this permission
+                    "The camera is important for this app. Please grant the permission."
+                } else {
+                    // If it's the first time the user lands on this feature, or the user
+                    // doesn't want to be asked again for this permission, explain that the
+                    // permission is required
+                    "Camera permission required for this feature to be available. " +
+                            "Please grant the permission"
+                }
+                Text(textToShow)
+                Button(onClick = { locationPermissionState.launchPermissionRequest() }) {
+                    Text("Request permission")
+                }
+            }
+        }
+
         GoogleMap(
             properties = viewModel.state.properties,
             uiSettings = mapUiSettings,
