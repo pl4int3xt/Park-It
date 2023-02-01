ad0cf321543231486f5ad9433f202e85985c91e8
BENJAH <benjahjp@gmail.com>
2023-02-01 13:18:02 +0300

    fix

--- app/src/main/java/com/example/modularization/presentation/map/components/MapScreen.kt
+++ app/src/main/java/com/example/modularization/presentation/map/components/MapScreen.kt
@@ -10,8 +10,11 @@ import androidx.compose.animation.core.MutableTransitionState
 import androidx.compose.animation.core.tween
 import androidx.compose.animation.slideInVertically
 import androidx.compose.animation.slideOutVertically
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.filled.MyLocation
@@ -27,8 +30,10 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
 import androidx.compose.ui.window.DialogProperties
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -185,8 +190,16 @@ fun MapScreen(
                         usePlatformDefaultWidth = false
                     )
                 ) {
-                    viewModel.state.parkingSpots.forEach { parkingSpot ->
-                        Text(text = parkingSpot.lng.toString())
+                    Column(
+                        modifier = Modifier.background(
+                            color = Color.White.copy(alpha = 0.5F),
+                            shape = RoundedCornerShape(10.dp)
+                        )
+                            .align(Ali)
+                    ) {
+                        viewModel.state.parkingSpots.forEach { parkingSpot ->
+                            Text(text = parkingSpot.lng.toString())
+                        }
                     }
                 }
             }
